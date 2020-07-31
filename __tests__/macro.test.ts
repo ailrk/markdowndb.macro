c1f7ed32faa19476e21886f2335cbbee8bef774d
Jimmy <jimmy123good@hotmail.com>
2020-07-30 18:21:46 -0700

    testing

--- __tests__/macro.test.ts
+++ __tests__/macro.test.ts
@@ -1,10 +1,23 @@
-import {MarkdownDB} from '../dist/types';
-it('test1', () => {
+import {MarkdownRuntimeDatabase} from '../dist/MarkdownMap';
+
+it('runtime article length', () => {
   const markdowndb = require('../dist/macro');
-  const db: MarkdownDB = markdowndb('markdown');
-  console.log(db);
+  const db: MarkdownRuntimeDatabase = markdowndb('markdown');
   expect(
     Array.from(db.values("default"))
       .filter(e => e.header.tag.length === 2).length)
     .toBe(1);
+});
+
+it('runtime promise', () => {
+  const markdowndb = require('../dist/macro');
+  const db: MarkdownRuntimeDatabase = markdowndb('markdown');
+  expect(
+    Array.from(db.values("default"))
+      .filter(e => {
+        let val = "";
+        e.content.then(m => {val = m;});
+        console.log(val);
+        return val === "test";
+      }).length).toBe(1);
 });
---
+++ markdown/markdown3.md
@@ -0,0 +1,6 @@
+-- date 2020-06-29
+-- tag onetag
+-- source https://wikipedia.com/yadayadayada an-awesome-book an-not-so-awesome-book
+-- title The rest will all be part of the title 3.
+;;
+test
