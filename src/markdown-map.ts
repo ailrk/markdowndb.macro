1330be0a8dd0b8f86197f759230a30dd8095e6a2
Jimmy <jimmy123good@hotmail.com>
2020-08-04 17:07:01 -0700

    common interface

--- src/markdown-map.ts
+++ src/markdown-map.ts
@@ -1,5 +1,7 @@
 import {MarkdownRaw, Markdown, MarkdownHeader, MarkdownDB, ViewType, MarkdownText, MarkdownMeta} from './types';
 import {htmlPath} from './preprocess/static-gen';
+import {flat} from './utils/flat';
+import {notUndefined} from './utils/type';
 
 export class MarkdownDatabase implements MarkdownDB {
   // different views of Markdowns, all constructed at compile time.
@@ -113,6 +115,60 @@ export class MarkdownStaticDatabase extends MarkdownDatabase {
     );
     this.tagView = metaview(indices.tag, f);
     this.timeView = metaview(indices.time, f);
+  }
+}
+
+export class AllDB implements MarkdownDB {
+  list?: Array<MarkdownDB> = undefined;
+
+  public constructor(list: Array<MarkdownDB>) {
+    this.list = list;
+  }
+  get(key: number): Markdown | undefined;
+  get(key: Date | string): Array<Markdown> | undefined;
+  get(key: number | Date | string):
+    | (Markdown | undefined)
+    | (Array<Markdown> | undefined) {
+    if (typeof key === "number") {
+      const filtered = this.list?.map(m => m.get(key)).filter(notUndefined);
+      if (filtered!.length > 1) {throw new Error("id not unique");}
+      return filtered?.pop();
+    }
+    return flat(this.list?.map(db => db.get(key)).filter(notUndefined)!);
+  }
+
+  entries(view: "default"): Array<[number, Markdown]> | undefined;
+  entries(view: "time" | "tag"): Array<[string, Array<Markdown>]> | undefined;
+  entries(view: ViewType):
+    | (Array<[number, Markdown]> | undefined)
+    | (Array<[string, Array<Markdown>]> | undefined) {
+    if (view === "default") return this.megaList(m => m.entries(view));
+    else return this.megaList(m => m.entries(view));
+  }
+
+  values(view: "default"): Array<Markdown> | undefined;
+  values(view: "time" | "tag"): Array<Array<Markdown>> | undefined;
+  values(view: ViewType):
+    | Array<Markdown> | undefined
+    | Array<Array<Markdown>> | undefined {
+    if (view === "default") return this.megaList(m => m.values(view));
+    else return this.megaList(m => m.values(view));
+  }
+
+  keys(view: "default"): Array<number> | undefined;
+  keys(view: "time" | "tag"): Array<string> | undefined;
+  keys(view: ViewType):
+    | Array<number> | undefined
+    | Array<string> | undefined {
+    if (view === "default") return this.megaList(m => m.keys(view));
+    else return this.megaList(m => m.keys(view));
+  }
+
+  megaList<T>(cb: (m: MarkdownDB) => Array<T> | undefined) {
+    const makeIteratorArray =
+      <T>(cb: (m: MarkdownDB) => Array<T> | undefined) =>
+        this.list!.map(cb)!.filter(notUndefined);
+    return flat(makeIteratorArray(cb));
   }
 }
 
--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,3 +1,7 @@
 export type IfEqual<T, U> =
   (<G>() => G extends T ? 1 : 2) extends
   (<G>() => G extends U ? 1 : 2) ? true : false;
+
+export function notUndefined<T>(x: T | undefined): x is T {
+  return x !== undefined;
+}
